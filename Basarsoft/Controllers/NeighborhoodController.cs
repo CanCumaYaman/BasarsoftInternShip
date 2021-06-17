8a325ba429a1ab150ac0f1fec51a17cf5b4857af
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-06-17 12:30:36 +0300

    Neighborhood added to map and neighborhood info sended to controller

--- Basarsoft/Controllers/NeighborhoodController.cs
+++ Basarsoft/Controllers/NeighborhoodController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Basarsoft.Business.Abstract;
+using Basarsoft.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +10,31 @@ namespace Basarsoft.Controllers
 {
     public class NeighborhoodController : Controller
     {
-       
+        private readonly INeighborhoodManager _neighborhoodManager;
+
+        public NeighborhoodController(INeighborhoodManager neighborhoodManager)
+        {
+            _neighborhoodManager = neighborhoodManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-      
+        [HttpPost]
+        public JsonResult SaveNeighborhood(Neighborhood neighborhood, string result, string no)
+        {
+            if (ModelState.IsValid)
+            {
+                neighborhood.Coordinates =result;
+                neighborhood.NeighborhoodName = no;
+                _neighborhoodManager.Add(neighborhood);
+
+            }
+            return Json("");
+        }
+
+
     }
 }
--- Basarsoft/wwwroot/js/neighborhood.js
+++ Basarsoft/wwwroot/js/neighborhood.js
@@ -16,19 +16,71 @@ function ActiveNeigh() {
 
 addDaireInteraction();
 
+
 add_neigh.on('drawend', function (e) {
+
+    var currentFeature = e.feature;
+
+    var _coords = currentFeature.getGeometry().getCoordinates();
+    add_neigh.setActive(false);
+
     jsPanel.create({
-        id: "mahalle_ekle_panel",
+        id: "neigh_add_panel",
         theme: 'success',
-        headerTitle: 'Mahalle Eklendi',
+        headerTitle: 'Add Neighborhood',
         position: 'center-top 0 58',
-        contentSize: '200 200',
-        content: 'Mahalle Çizim İşlemi Başarılı',
+        contentSize: '300 250',
+        content: 'No: <input id="neigh_no" type="text"/><br><br><br><button style="height:40px;width:60px" id="neigh_add" class="btn btn-success">Add</button>',
         callback: function () {
             this.content.style.padding = '20px';
         }
     });
+    document.getElementById('neigh_add').onclick = function () {
 
-    add_neigh.setActive(false);
+        var _no = $('#neigh_no').val();
 
+        if (_no.length < 1) {
+
+            alert("Please enter neighborhood name");
+
+            return;
+        }
+        var i = 0;
+        var result = "";
+        while (i < 1) {
+            for (j = 0; j < _coords[0].length; j++) {
+                if (j == _coords[0].length - 1) {
+                    result = result + _coords[i][j];
+                    
+                } else {
+                    result = result + _coords[i][j] + "*";
+                }
+                
+            }
+            i = 1;
+        }
+
+        var _data = {
+            result: result,
+            no: _no
+        };
+        $.ajax({
+            type: "POST",
+            url: "/Neighborhood/SaveNeighborhood",
+            dataType: 'json',
+            data: _data,
+            success: function (message) {
+                alert("Successfully added");
+
+                door.setActive(false);
+            },
+
+            error: function () {
+                alert("Something went wrong");
+            },
+            onbeforeclose: function () {
+                return onbeforeclose();
+            },
+        });
+    }
 });
